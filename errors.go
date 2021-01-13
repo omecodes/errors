8eda8b62d63f820ea724af61b4dcb29a5f14ea49
omecodes <omecodes@gmail.com>
2021-01-13 21:54:49 +0100

    Turned details into a set of info

--- errors.go
+++ errors.go
@@ -19,17 +19,15 @@ const (
 	DuplicateResource   = 9
 )
 
-type Details map[string]string
-
 type Info struct {
 	Name    string
 	Details string
 }
 
 type Error struct {
-	Code    int     `json:"code,omitempty"`
-	Message string  `json:"message,omitempty"`
-	Details Details `json:"details,omitempty"`
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+	Details []Info `json:"details,omitempty"`
 }
 
 func (e Error) Error() string {
@@ -43,15 +41,8 @@ func (e Error) Error() string {
 	return fmt.Sprintf("{\"code\":%d, \"message\":\"%s\"}", e.Code, e.Message)
 }
 
-func (e Error) SetDetails(details Details) {
-	e.Details = details
-}
-
-func (e Error) AddDetails(name, value string) {
-	if e.Details == nil {
-		e.Details = Details{}
-	}
-	e.Details[name] = value
+func (e Error) AppendDetails(info ...Info) {
+	e.Details = append(e.Details, info...)
 }
 
 func (e Error) HTTPStatus() int {
@@ -77,16 +68,24 @@ func (e Error) HTTPStatus() int {
 	}
 }
 
+func AppendDetails(err error, info ...Info) Error {
+	if e, ok := err.(Error); ok {
+		e.AppendDetails(info...)
+		return e
+	}
+
+	msg := err.Error()
+	e, _ := Parse(msg)
+	e.AppendDetails(info...)
+	return e
+}
+
 func Create(code int, message string, info ...Info) *Error {
 	e := &Error{
 		Code:    code,
 		Message: message,
-		Details: Details{},
 	}
-
-	for _, i := range info {
-		e.Details[i.Name] = i.Details
-	}
+	e.Details = append(e.Details, info...)
 	return e
 }
 
@@ -94,15 +93,32 @@ func New(msg string) Error {
 	return Error{Message: msg}
 }
 
+func Parse(str string) (Error, bool) {
+	var e Error
+	err := json.Unmarshal([]byte(str), &e)
+	return e, err == nil && e.Code > 0 && e.Message != ""
+}
+
 func IsNotFound(e error) bool {
 	if err, ok := e.(Error); ok {
-		return err.Code == NotFound || err.Code == ServiceUnavailable
+		return err.Code == NotFound
 	}
 
 	var err Error
 	_ = json.Unmarshal([]byte(e.Error()), &err)
 
-	return err.Code == NotFound || err.Code == ServiceUnavailable
+	return err.Code == NotFound
+}
+
+func IsServiceUnavailable(e error) bool {
+	if err, ok := e.(Error); ok {
+		return err.Code == ServiceUnavailable
+	}
+
+	var err Error
+	_ = json.Unmarshal([]byte(e.Error()), &err)
+
+	return err.Code == ServiceUnavailable
 }
 
 func IsDuplicate(e error) bool {
@@ -116,13 +132,25 @@ func IsDuplicate(e error) bool {
 	return err.Code == DuplicateResource
 }
 
-func IsPermissionDenied(e error) bool {
+func IsUnauthorized(e error) bool {
 	if err, ok := e.(Error); ok {
-		return err.Code == Forbidden || err.Code == Unauthorized
+		return err.Code == Unauthorized
 	}
 
 	var err Error
 	_ = json.Unmarshal([]byte(e.Error()), &err)
 
-	return err.Code == Forbidden || err.Code == Unauthorized
+	return err.Code == Unauthorized
+}
+
+func IsForbidden(e error) bool {
+
+	if err, ok := e.(Error); ok {
+		return err.Code == Forbidden
+	}
+
+	var err Error
+	_ = json.Unmarshal([]byte(e.Error()), &err)
+
+	return err.Code == Forbidden
 }
