08b639f2457b0bea7dcf3adc0b5ae7c705859c88
omecodes <omecodes@gmail.com>
2021-07-07 22:37:34 +0200

    Rewrite errors and use well defined values

--- errors.go
+++ errors.go
@@ -2,248 +2,110 @@ package errors
 
 import (
 	"encoding/json"
-	"fmt"
-
-	"github.com/go-sql-driver/mysql"
-	"github.com/mattn/go-sqlite3"
+	"errors"
+	"io"
 )
 
-const (
-	CodeInternal = iota
-	CodeBadRequest
-	CodeUnauthorized
-	CodeForbidden
-	CodeNotFound
-	CodeConflict
-	CodeUnImplemented
-	CodeServiceUnavailable
-	CodeVersionNotSupported
-	CodeUnReferencedID
-	CodeUnSupported
+var (
+	ErrBadInput           = errors.New("bad input")
+	ErrForbidden          = errors.New("forbidden")
+	ErrUnauthorized       = errors.New("forbidden")
+	ErrInternal           = errors.New("internal")
+	ErrNotSupported       = errors.New("unsupported")
+	ErrServiceUnavailable = errors.New("service unavailable")
+	ErrUnImplemented      = errors.New("unimplemented")
+	ErrNotFound           = errors.New("not found")
+	ErrConflict           = errors.New("conflict")
 )
 
-type Details struct {
-	Key   string      `json:"key,omitempty"`
-	Value interface{} `json:"value,omitempty"`
+func New(message string) error {
+	return Detailed(ErrInternal, With("message", message))
 }
 
-type Error struct {
-	Code    int       `json:"code,omitempty"`
-	Message string    `json:"message,omitempty"`
-	Details []Details `json:"details,omitempty"`
+type jsonObject map[string]interface{}
+
+type detailed struct {
+	err     error
+	details map[string]interface{}
 }
 
-func (e *Error) Error() string {
-	encoded, err := json.Marshal(e)
-	if err != nil {
-		return fmt.Sprintf("{\"code\":%d, \"message\":\"%s\"}", e.Code, e.Message)
+func (d *detailed) Error() string {
+	return d.err.Error()
+}
+
+func (d *detailed) Is(err error) bool {
+	return errors.Is(d.err, err)
+}
+
+type Info func(err *detailed)
+
+func With(key string, value interface{}) Info {
+	return func(err *detailed) {
+		if err.details == nil {
+			err.details = make(map[string]interface{})
+		}
+		err.details[key] = value
 	}
-	return string(encoded)
 }
 
-func (e *Error) AddDetails(key string, value interface{}) {
-	e.Details = append(e.Details, Details{Key: key, Value: value})
+func Detailed(err error, info ...Info) error {
+	d := &detailed{
+		err: err,
+	}
+	for _, i := range info {
+		i(d)
+	}
+	return d
 }
 
-func (e *Error) HTTPStatus() int {
-	switch e.Code {
-	case CodeBadRequest:
+func Write(w io.Writer, err error) (int, error) {
+	var (
+		data []byte
+		mErr error
+	)
+
+	d, ok := err.(*detailed)
+	if ok {
+		data, mErr = json.Marshal(jsonObject{
+			"code":    GetHttpStatusCode(d.err),
+			"details": d.details,
+		})
+	} else {
+		data, mErr = json.Marshal(jsonObject{
+			"error": err.Error(),
+		})
+	}
+
+	if mErr != nil {
+		return 0, err
+	}
+	return w.Write(data)
+}
+
+func GetHttpStatusCode(err error) int {
+	d, ok := err.(*detailed)
+	if ok {
+		return GetHttpStatusCode(d.err)
+	}
+
+	switch err {
+	case ErrBadInput:
 		return 400
-	case CodeUnauthorized:
+	case ErrUnauthorized:
 		return 401
-	case CodeForbidden:
+	case ErrForbidden:
 		return 403
-	case CodeNotFound:
+	case ErrNotFound:
 		return 404
-	case CodeConflict:
+	case ErrConflict:
 		return 409
-	case CodeUnImplemented:
+	case ErrUnImplemented:
 		return 501
-	case CodeServiceUnavailable:
+	case ErrServiceUnavailable:
 		return 503
-	case CodeVersionNotSupported:
+	case ErrNotSupported:
 		return 505
 	default:
 		return 500
 	}
-}
-
-func HTTPStatus(err error) int {
-	e, ok := err.(*Error)
-	if ok {
-		return e.HTTPStatus()
-	}
-
-	e, ok = Parse(err.Error())
-	if ok {
-		return e.HTTPStatus()
-	}
-
-	return 500
-}
-
-func Parse(str string) (*Error, bool) {
-	var e *Error
-	err := json.Unmarshal([]byte(str), &e)
-	return e, err == nil && e.Code > 0 && e.Message != ""
-}
-
-func IsNotFound(e error) bool {
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeNotFound
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-	return err.Code == CodeNotFound
-}
-
-func IsServiceUnavailable(e error) bool {
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeServiceUnavailable
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-
-	return err.Code == CodeServiceUnavailable
-}
-
-func IsConflict(e error) bool {
-	if isPrimaryKeyConstraintError(e) {
-		return true
-	}
-
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeConflict
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-
-	return err.Code == CodeConflict
-}
-
-func IsNotReferencedID(e error) bool {
-	if isForeignKeyConstraintError(e) {
-		return true
-	}
-
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeUnReferencedID
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-
-	return err.Code == CodeUnReferencedID
-}
-
-func IsUnauthorized(e error) bool {
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeUnauthorized
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-
-	return err.Code == CodeUnauthorized
-}
-
-func IsForbidden(e error) bool {
-
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeForbidden
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-
-	return err.Code == CodeForbidden
-}
-
-func IsUnSupported(e error) bool {
-	if err, ok := e.(*Error); ok {
-		return err.Code == CodeUnSupported
-	}
-
-	var err Error
-	_ = json.Unmarshal([]byte(e.Error()), &err)
-
-	return err.Code == CodeUnSupported
-}
-
-func New(message string, details ...Details) *Error {
-	return &Error{Code: CodeInternal, Message: message, Details: details}
-}
-
-func Internal(message string, details ...Details) *Error {
-	return &Error{Code: CodeInternal, Message: message, Details: details}
-}
-
-func BadRequest(message string, details ...Details) *Error {
-	return &Error{Code: CodeBadRequest, Message: message, Details: details}
-}
-
-func Unauthorized(message string, details ...Details) *Error {
-	return &Error{Code: CodeUnauthorized, Message: message, Details: details}
-}
-
-func Forbidden(message string, details ...Details) *Error {
-	return &Error{Code: CodeForbidden, Message: message, Details: details}
-}
-
-func NotFound(message string, details ...Details) *Error {
-	return &Error{Code: CodeNotFound, Message: message, Details: details}
-}
-
-func Conflict(message string, details ...Details) *Error {
-	return &Error{Code: CodeConflict, Message: message, Details: details}
-}
-
-func UnImplemented(message string, details ...Details) *Error {
-	return &Error{Code: CodeUnImplemented, Message: message, Details: details}
-}
-
-func ServiceUnavailable(message string, details ...Details) *Error {
-	return &Error{Code: CodeServiceUnavailable, Message: message, Details: details}
-}
-
-func VersionNotSupported(message string, details ...Details) *Error {
-	return &Error{Code: CodeVersionNotSupported, Message: message, Details: details}
-}
-
-func UnReferencedID(message string, details ...Details) *Error {
-	return &Error{Code: CodeUnReferencedID, Message: message, Details: details}
-}
-
-func Unsupported(message string, details ...Details) *Error {
-	return &Error{Code: CodeUnSupported, Message: message, Details: details}
-}
-
-func isPrimaryKeyConstraintError(err error) bool {
-	if me, ok := err.(*mysql.MySQLError); ok {
-		return me.Number == 1062
-
-	} else if se, ok := err.(sqlite3.Error); ok {
-		return se.ExtendedCode == 2067 || se.ExtendedCode == 1555
-	}
-	return false
-}
-
-func isForeignKeyConstraintError(err error) bool {
-	if me, ok := err.(*mysql.MySQLError); ok {
-		return me.Number == 1216
-
-	} else if se, ok := err.(sqlite3.Error); ok {
-		return se.ExtendedCode == sqlite3.ErrConstraintForeignKey
-	}
-	return false
-}
-
-func isForeignKeyDuplicate(err error) bool {
-	if me, ok := err.(*mysql.MySQLError); ok {
-		return me.Number == 1826
-	}
-	return false
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/omecodes/errors
 
 go 1.15
-
-require (
-	github.com/go-sql-driver/mysql v1.5.0
-	github.com/mattn/go-sqlite3 v1.14.6
-)
